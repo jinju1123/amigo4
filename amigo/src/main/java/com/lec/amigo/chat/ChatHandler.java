17ccb82976aaa5d8cb014cb4a47b1cdaa1f29cf9
DESKTOP-O03MT84\ezen <doc9733@gmail.com>
2023-01-17 17:54:51 +0900

    20230117

--- amigo/src/main/java/com/lec/amigo/chat/ChatHandler.java
+++ amigo/src/main/java/com/lec/amigo/chat/ChatHandler.java
@@ -192,13 +192,8 @@ public class ChatHandler extends TextWebSocketHandler{
 		
 			
 		}
-<<<<<<< HEAD
 
-=======
-		*/
-		
-		
-	}
+			
 	
 	private String getId(WebSocketSession session) {
 		Map<String, Object> httpSession = session.getAttributes();
@@ -210,7 +205,6 @@ public class ChatHandler extends TextWebSocketHandler{
 		}else return loginUser.getUser_name();
 		
 	}
->>>>>>> c07e54093199167f01e321ce98dffbab09b9ae63
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
