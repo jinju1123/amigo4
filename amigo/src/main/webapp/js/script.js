a139aa9f615884045bff989852771decf31fc502
snoopy1224 <misty12024@gmail.com>
2023-01-18 23:48:50 +0900

    All footer, css, js, new myprofile upgrade

--- amigo/src/main/webapp/js/script.js
+++ amigo/src/main/webapp/js/script.js
@@ -71,6 +71,24 @@ function selectAll(selectAll) {
   });
 }
 
+/* myprofile 사진업로드 */
+$(document).ready(function () {
+  // Prepare the preview for profile picture
+  $("#wizard-picture").change(function () {
+    readURL(this);
+  });
+});
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $("#wizardPicturePreview").attr("src", e.target.result).fadeIn("slow");
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
 // Add active class on another page linked
 // ==========================================
 // $(window).on('load',function () {
